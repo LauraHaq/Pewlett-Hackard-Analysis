49ed272cb374c8cc8ac1f86d3c28eb0e64425688
LauraHaq <86267773+LauraHaq@users.noreply.github.com>
2021-08-16 00:48:06 -0700

    add fourth result

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -52,3 +52,24 @@ ON e.emp_no = t.emp_no
 WHERE (de.to_date = ('9999-01-01'))
 AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+-- FIND A FOURTH RESULT
+SELECT DISTINCT ON(emp_no) emp_no, 
+		dept_no, 
+		to_date
+INTO dept_affected
+FROM dept_emp
+ORDER BY emp_no ASC, to_date DESC;
+
+SELECT ut.emp_no, ut.title, da.dept_no, da.to_date, d.dept_name
+INTO dept_unique
+FROM unique_titles as ut
+LEFT JOIN dept_affected as da
+ON ut.emp_no = da.emp_no
+LEFT JOIN departments as d
+ON da.dept_no = d.dept_no;
+
+SELECT COUNT(emp_no) AS emp_count, dept_name
+FROM dept_unique
+GROUP BY dept_name
+ORDER BY emp_count DESC;
